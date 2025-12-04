b9983944166df6e64f7fcec7be1aeb4bc363f3a9
Seeya Vhora <seeyavhora5557@gmail.com>
2025-12-05 01:21:27 +0530

    js updated

--- app.js
+++ app.js
@@ -9,10 +9,12 @@ var searchText = '';
 var sortBy = 'time-asc';
 var lastDeleted = null;
 var searchTimeout;
+var editingId = null;
 
 // setup everything when page loads
-window.onload = function() {
+window.onload = function () {
   loadReminders();
+  startTimerLoop();
   setupEventListeners();
   checkNotificationPermission();
   render();
@@ -21,43 +23,71 @@ window.onload = function() {
 // event listeners
 function setupEventListeners() {
   // notification button
-  document.getElementById('btn-permission').onclick = function() {
-    askNotificationPermission();
-  };
+  var btnPermission = document.getElementById('btn-permission');
+  if (btnPermission) {
+    btnPermission.onclick = function () {
+      askNotificationPermission();
+    };
+  }
+
+  // test notification button
+  var btnTest = document.getElementById('btn-test-notification');
+  if (btnTest) {
+    btnTest.onclick = function () {
+      if (Notification.permission === 'default') {
+        askNotificationPermission();
+      } else {
+        showNotification('Test Notification', 'This is a test to ensure notifications are working!');
+      }
+    };
+  }
 
   // form submit
-  document.getElementById('reminder-form').onsubmit = function(e) {
-    e.preventDefault();
-    addReminder();
-  };
+  var form = document.getElementById('reminder-form');
+  if (form) {
+    form.onsubmit = function (e) {
+      e.preventDefault();
+      addReminder();
+    };
+  }
 
   // search box
-  document.getElementById('search-input').oninput = function(e) {
-    clearTimeout(searchTimeout);
-    searchTimeout = setTimeout(function() {
-      searchText = e.target.value.toLowerCase();
-      currentPage = 1;
-      render();
-    }, 300);
-  };
+  var searchInput = document.getElementById('search-input');
+  if (searchInput) {
+    searchInput.oninput = function (e) {
+      clearTimeout(searchTimeout);
+      searchTimeout = setTimeout(function () {
+        searchText = e.target.value.toLowerCase();
+        currentPage = 1;
+        render();
+      }, 300);
+    };
+  }
 
   // sort dropdown
-  document.getElementById('sort-select').onchange = function(e) {
-    sortBy = e.target.value;
-    currentPage = 1;
-    render();
-  };
+  var sortSelect = document.getElementById('sort-select');
+  if (sortSelect) {
+    sortSelect.onchange = function (e) {
+      sortBy = e.target.value;
+      currentPage = 1;
+      render();
+    };
+  }
 
   // undo button
-  document.getElementById('btn-undo').onclick = function() {
-    undoDelete();
-  };
+  var btnUndo = document.getElementById('btn-undo');
+  if (btnUndo) {
+    btnUndo.onclick = function () {
+      undoDelete();
+    };
+  }
 
   // keyboard shortcut
-  document.onkeydown = function(e) {
+  document.onkeydown = function (e) {
     if (e.ctrlKey && e.key === 'z') {
       e.preventDefault();
       undoDelete();
     }
   };
 }
+
